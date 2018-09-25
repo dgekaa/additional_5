7be44995036e3b4c3ef9c085618d5fedbf572d76
dgekaa <dgeka_vip@mail.ru>
2018-09-25 22:08:38 +0300

    new 5 comm

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  		var arr = str.split("");
+		var count = 0;
+		var brack = bracketsConfig;
+		
+		for(var i = 0; i < arr.length ; i++){
+			for(var j = 0; j < brack.length ; j++){
+				if(arr[i]===brack[j][0] && arr[i+2]===brack[j][1]&&arr[i+1]!==arr[i]&&arr[i+1]!==arr[i+2]){
+					return false;
+				}else if(arr[i] === brack[j][0] && arr[i] !== "|"){
+					count++;
+				}else if(arr[i] === brack[j][1]&&arr[i] !== "|"){
+					count--;
+					if(count<0){
+						return false;
+					}
+				}
+			}
+		}
+
+		if(count===0){
+			return true;
+		}else{
+			return false;
+		}
 }
